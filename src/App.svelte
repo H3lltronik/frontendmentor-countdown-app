8244f082a6e16c809bd964bce5d53c6ee0c95175
H3lltronik <esau.egs1@gmail.com>
2021-07-16 22:25:40 -0500

    begin

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,35 @@
-<script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
-</script>
+<main class="app">
+  <div class="app__contents">
+    <div class="app__launcher">
+      <h1 class="app_title">WE'RE LAUNCHING SOON</h1>
+    </div>
+  </div>
+  <div class="app__footer">
+    <div class="app__footer_wrapper">
+      <div class=""><img src={HillsImg} alt=""></div>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+      <div class="app__footer_icons">
+        <a target="_blank" href="https://www.facebook.com/">
+          <Icon size="1em" path={ mdiFacebook }></Icon>
+        </a>
+        <a target="_blank" href="https://www.pinterest.com/">
+          <Icon size="1em" path={ mdiPinterest }></Icon>
+        </a>
+        <a target="_blank" href="https://www.instagram.com/">
+          <Icon size="1em" path={ mdiInstagram }></Icon>
+        </a>
+      </div>
+    </div>
+  </div>
 </main>
 
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+<script lang="ts">
+  import Icon from 'mdi-svelte';
+  import HillsImg from './assets/images/pattern-hills.svg'
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+  import { mdiFacebook, mdiPinterest, mdiInstagram } from '@mdi/js';
+</script>
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+<style lang="scss" global>
+  @import "./assets/mains.scss";
 </style>
